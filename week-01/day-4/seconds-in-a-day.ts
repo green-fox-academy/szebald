ce15b44d21d871755b1ac6c1081defa56bd89010
szebald <konczkristof@icloud.com>
2019-04-04 16:43:08 +0200

    now I know how many secs are remained from this day

---
+++ week-01/day-4/seconds-in-a-day.ts
@@ -0,0 +1,13 @@
+'use strict';
+
+let currentHours: number = 14;
+let currentMinutes: number = 34;
+let currentSeconds: number = 42;
+
+// Write a program that prints the remaining seconds (as an integer) from a
+// day if the current time is represented by these variables
+
+let secsinaday: number = 82600;
+let secsnow: number = currentSeconds + (60 * currentMinutes) + (60 * 60 * currentHours);
+
+console.log(secsinaday - secsnow)
