f9dd4833a52f44bd8b645ec81ab3734f1fce37b9
szebald <konczkristof@icloud.com>
2019-04-04 15:52:26 +0200

    accidently updated with a space lol

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = "Kristof"
+let myAge: number = 23
+let myHeight: number = 1.83
+let amIMarried: boolean = false;
+
+console.log("My name is " + myName + ". I am " + myAge +" and " + myHeight + " high.")
+
+if (amIMarried)
+    console.log("I am married.")
+else(!amIMarried)
+    console.log("I am not married.")
