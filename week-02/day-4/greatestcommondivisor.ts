b7536bdfb4fa9fd2fa60daf570c666ebb6f8a56b
szebald <konczkristof@icloud.com>
2019-04-11 13:53:59 +0200

    changed the method

--- week-02/day-4/greatestcommondivisor.ts
+++ week-02/day-4/greatestcommondivisor.ts
@@ -3,11 +3,13 @@
 //Find the greatest common divisor of two numbers using recursion.
 
 function greatestc (x: number, y: number): any {
-    if (!y){
-        return x
-    }
-    else {
-        return greatestc(y, x % y)
-    }
-}
-console.log(greatestc(4, 2))
+//     if (!y){
+//         return x
+//     }
+//     else {
+//         return greatestc(y, x % y)
+//     }
+// }
+
+return !y ? x : greatestc(y, x % y);}
+console.log(greatestc(18, 9))
