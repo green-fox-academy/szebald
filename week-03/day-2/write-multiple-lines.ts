5095bc2f6b4c37fac1ff8b0631c2aefa1a764ec2
szebald <konczkristof@icloud.com>
2019-04-16 17:11:57 +0200

    feat(write-multiple-lines.ts) add a string many times to a file

---
+++ week-03/day-2/write-multiple-lines.ts
@@ -0,0 +1,29 @@
+'use strict'
+export { }
+
+// Create a function that takes 3 parameters: a path, a word and a number
+// and is able to write into a file.
+// The path parameter should be a string that describes the location of the file you wish to modify
+// The word parameter should also be a string that will be written to the file as individual lines
+// The number parameter should describe how many lines the file should have.
+// If the word is 'apple' and the number is 5, it should write 5 lines
+// into the file and each line should read 'apple'
+// The function should not raise any errors if it could not write the file.
+
+const fs = require('fs')
+
+function writeLines(path: string, word: string, number: number) {
+  try {
+    let content = word + '\r';
+    fs.writeFileSync(path, content);
+    for (let i = 1; i < number; i++) {
+      fs.appendFileSync(path, content);
+    }
+  }
+  catch (e) {
+
+  }
+};
+
+
+writeLines('./my-file.txt', "szakadjmeg", 3);
