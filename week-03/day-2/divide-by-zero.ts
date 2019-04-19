d7b1277783ea7c567bac4b095dfc71ca5d51a4c0
szebald <konczkristof@icloud.com>
2019-04-19 15:57:41 +0200

    feat(divby0): add try/catch feature

--- week-03/day-2/divide-by-zero.ts
+++ week-03/day-2/divide-by-zero.ts
@@ -1,16 +1,24 @@
 'use strict'
+export{}
 
 // Create a function that takes a number
 // divides ten with it,
 // and prints the result.
 // It should print 'fail' if the parameter is 0
 
-function divider (n: number = 100) {
-    if (n !== 0) {
-      console.log(100 / n);
-    }else {
-    console.log('fail');
-  }
-  }
-  
-  divider(0)
+let n: number = 0;
+function div(n: number): any {
+    return 10 / n;
+}
+try {
+    if (n === 0) {
+        throw new Error('fail')
+    }
+    else if (n > 0){
+        console.log(div(n))
+    }
+
+}
+catch (e) {
+    console.log(e.message)
+}
