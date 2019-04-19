c3e5d9add646b1be07eeaa2b969e7744f70f8626
szebald <konczkristof@icloud.com>
2019-04-19 15:59:13 +0200

    feat(triangle): minor fixes

--- week-02/day-5/triangle.ts
+++ week-02/day-5/triangle.ts
@@ -1,27 +1,27 @@
 'use strict'
 
-const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const canvas = document.querySelector('.main-canvas') as HTMLcanvasElement;
 const ctx = canvas.getContext('2d');
 
-function triangle(Ax, Ay, Bx, By, Cx, Cy, d) {
+function triangle(aX, aY, bX, bY, cX, cY, d) {
   if (d === 0) {
-    ctx.moveTo(Ax, Ay);
-    ctx.lineTo(Bx, By);
-    ctx.lineTo(Cx, Cy);
-    ctx.lineTo(Ax, Ay);
+    ctx.moveTo(aX, aY);
+    ctx.lineTo(bX, bY);
+    ctx.lineTo(cX, cY);
+    ctx.lineTo(aX, aY);
     ctx.strokeStyle = 'black';
     ctx.stroke();
 
   }
   else {
-    triangle(Ax, Ay, (Ax + Cx) / 2, (Ay + Cy) / 2, (Ax + Bx) / 2, (Ay + By) / 2, d - 1);
-    triangle((Ax + Bx) / 2, (Ay + By) / 2, (Bx + Cx) / 2, (By + Cy) / 2, Bx, By, d - 1);
-    triangle((Ax + Cx) / 2, (Ay + Cy) / 2, (Bx + Cx) / 2, (By + Cy) / 2, Cx, Cy, d - 1);
+    triangle(aX, aY, (aX + cX) / 2, (aY + cY) / 2, (aX + bX) / 2, (aY + bY) / 2, d - 1);
+    triangle((aX + bX) / 2, (aY + bY) / 2, (bX + cX) / 2, (bY + cY) / 2, bX, bY, d - 1);
+    triangle((aX + cX) / 2, (aY + cY) / 2, (bX + cX) / 2, (bY + cY) / 2, cX, cY, d - 1);
   }
 }
 
 triangle(50, 20, 450, 20, 250, 380, 6);
-ctx.fillStyle = 'yellow';
+ctx.fillStyle = 'orange';
 ctx.fill();
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 0.1;
